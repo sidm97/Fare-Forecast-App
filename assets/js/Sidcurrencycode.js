790efb2b0edd3719c9398e69b2e66c7ea0693d93
sidm97 <sid.m.974@gmail.com>
2024-02-05 14:27:50 +0000

    added the first section of the submit button event listener. This will likely change to a submit listener. this part essentially creates the url that will be used for the first api, the geocoding one.

--- assets/js/Sidcurrencycode.js
+++ assets/js/Sidcurrencycode.js
@@ -6,3 +6,11 @@ const databaseLink = "https://pkgstore.datahub.io/core/country-codes/country-cod
 let searchString;
 let querylocationURL;
 let querycurrencyURL;
+
+// user submits city --> geocoding API takes the city and returns a country code --> another database, link below, takes country code and returns a currency code --> currency exchange API takes this currency code to return conversion ratez
+$("#submit").on("click", function () {
+    $(".exchange_rate").empty();
+    searchString = $("#city").val().toUpperCase();
+    console.log("searching for: " + searchString);
+    querylocationURL = locationAPI + searchString + locationAPIkey;
+    console.log("URL for weather API is : " + querylocationURL);
