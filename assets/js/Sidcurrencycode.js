731f616a26447b9a3a8569a559d720041cd537a0
sidm97 <sid.m.974@gmail.com>
2024-02-06 19:27:34 +0000

    Shazna has added HTML and CSS pages, I have begun integrating my JS files into it. The first problem is using the input form

--- assets/js/Sidcurrencycode.js
+++ assets/js/Sidcurrencycode.js
@@ -1,7 +1,7 @@
 // Sid's currency exchange code
-const currencyAPI = 'https://v6.exchangerate-api.com/v6/[add key here]/pair/gbp/';
+const currencyAPI = 'https://v6.exchangerate-api.com/v6/cce8439a497e07a97ad9f20f/pair/gbp/';
 const locationAPI = "http://api.openweathermap.org/geo/1.0/direct?q=";
-const locationAPIkey = "&appid=[add key here]";
+const locationAPIkey = "&appid=7029ec148d2666f47569499650a8ea2e";
 const databaseLink = "https://pkgstore.datahub.io/core/country-codes/country-codes_json/data/616b1fb83cbfd4eb6d9e7d52924bb00a/country-codes_json.json";
 let searchString;
 let querylocationURL;
