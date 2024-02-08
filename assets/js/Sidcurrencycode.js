c57468094d7dcaa7571c1b1d3fa242fe4a434370
sidm97 <sid.m.974@gmail.com>
2024-02-08 15:02:06 +0000

    error with http vs https for geocoding api, fixed hopefully on deploy

--- assets/js/Sidcurrencycode.js
+++ assets/js/Sidcurrencycode.js
@@ -1,6 +1,6 @@
 // Sid's currency exchange code
 const currencyAPI = 'https://v6.exchangerate-api.com/v6/cce8439a497e07a97ad9f20f/pair/';
-const locationAPI = "http://api.openweathermap.org/geo/1.0/direct?q=";
+const locationAPI = "https://api.openweathermap.org/geo/1.0/direct?q=";
 const locationAPIkey = "&appid=7029ec148d2666f47569499650a8ea2e";
 const databaseLink = "https://pkgstore.datahub.io/core/country-codes/country-codes_json/data/616b1fb83cbfd4eb6d9e7d52924bb00a/country-codes_json.json";
 let destinationString;
